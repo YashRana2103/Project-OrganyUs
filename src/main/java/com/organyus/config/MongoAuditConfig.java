e927a29129cafd5d3b734663da07af5bc2aedd84
Yash <yashrana21032006@gmail.com>
2025-06-20 18:49:09 +0530

    user schema/collection changed

---
+++ src/main/java/com/organyus/config/MongoAuditConfig.java
@@ -0,0 +1,26 @@
+package com.organyus.config;
+
+import org.bson.types.ObjectId;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+
+import java.util.Optional;
+
+@Configuration
+public class MongoAuditConfig {
+
+    @Bean
+    public AuditorAware<String> auditorProvider() {
+        // You can plug in logged-in user here if using Spring Security
+        return () -> Optional.of("System"); // or return Optional.of(userId);
+    }
+
+    // TODO: see how to manage this userId
+//    @Bean
+//    public AuditorAware<String> auditorProvider(ObjectId userId) {
+//        // You can plug in logged-in user here if using Spring Security
+//        return () -> Optional.of(String.valueOf(userId));
+//    }
+
+}
--- src/main/java/com/organyus/user/User.java
+++ src/main/java/com/organyus/user/User.java
@@ -1,14 +1,28 @@
 package com.organyus.user;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.bson.types.ObjectId;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
 
 @Document(collection = "users")
 @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
     @Id
     private ObjectId id;
@@ -22,15 +36,29 @@ public class User {
 
     @Email
     @NotBlank
+    @Indexed(unique = true)
     private String email;
 
     @NotBlank
-    private String passwordHash;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY) // for not returning it in response accidentally
+//    @JsonIgnore
+    private String hashedPassword;
 
-//    @NotBlank
+    @NotBlank
+    @Pattern(regexp = "\\d{10}", message = "Must be a valid 10-digit number")
     private String contactNumber;
 
-    private enum Status{
-        active, inactive, terminated
+    @Indexed
+    private Status status = Status.ACTIVE;
+
+    @Indexed
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    public enum Status{
+        ACTIVE, INACTIVE, TERMINATED
     }
 }
